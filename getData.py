633c49a0b520b06d2a33473d0ab44eda4979cbf0
NishantTheProgrammer <knishant113@gmail.com>
2020-04-04 14:43:17 +0530

    worldometer in not in whilelist of pythonanywhere that's why I'm trying an API

--- getData.py
+++ getData.py
@@ -1,21 +1,12 @@
-import bs4
-import requests
-
-url = "https://www.worldometers.info/coronavirus/#countries"
-data = requests.get(url)
-soup = bs4.BeautifulSoup(data.text, 'html.parser')
-
-table = soup.find_all('table')[0]
-
+import COVID19Py
+covid19 = COVID19Py.COVID19()
+locations = covid19.getLocations()
 
 data = []
+for i in range(len(locations)):
+    country = locations[i]['country']
+    confirmed = locations[i]['latest']['confirmed']
+    deaths = locations[i]['latest']['deaths']
+    data.append((country, confirmed, deaths))
 
-for tr in table.find_all('tr')[1:-1]:
-
-    name = tr.find_all('td')[0].text.strip()
-    total_case = tr.find_all('td')[1].text.strip()
-    new_case = tr.find_all('td')[2].text.strip()
-    deaths = tr.find_all('td')[3].text.strip()
-    data.append((name, total_case, new_case, deaths))
-
-print(*data, sep="\n")
+print(data)
