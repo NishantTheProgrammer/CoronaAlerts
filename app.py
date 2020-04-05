e5c62b09db48af8ad1117567a47438cda5c7aadd
NishantTheProgrammer <knishant113@gmail.com>
2020-04-05 22:42:09 +0530

    Added database connection for registering users Phone numbers

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
 from flask import Flask, request, render_template
 from twilio.twiml.messaging_response import MessagingResponse
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route("/")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1234@localhost/corona'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    phone = db.Column(db.String(20), primary_key=True)
+
+@app.route("/", methods = ['GET', 'POST'])
 def hello():
+
+    if(request.method == 'POST'):
+        """
+            Add Entry to database
+        """
+        phone = request.form.get('phone')
+        entry = User(phone = phone)
+        db.session.add(entry)
+        db.session.commit()
+
+
     return render_template('index.html')
+
+app.run(debug=True)
