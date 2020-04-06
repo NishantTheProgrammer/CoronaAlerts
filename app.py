9360fb7330554b674f2de5922e055f24192b7d9d
NishantTheProgrammer <knishant113@gmail.com>
2020-04-06 11:36:15 +0530

    Getting counries list with API on page

--- app.py
+++ app.py
@@ -1,28 +1,40 @@
 from flask import Flask, request, render_template
 from twilio.twiml.messaging_response import MessagingResponse
 from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy.exc
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1234@localhost/corona'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://NishantTheProgra:mypassword@NishantTheProgrammer.mysql.pythonanywhere-services.com/NishantTheProgra$Corona'
 db = SQLAlchemy(app)
 
 class User(db.Model):
     phone = db.Column(db.String(20), primary_key=True)
 
 @app.route("/", methods = ['GET', 'POST'])
-def hello():
+def index():
 
     if(request.method == 'POST'):
         """
             Add Entry to database
         """
-        phone = request.form.get('phone')
-        entry = User(phone = phone)
-        db.session.add(entry)
-        db.session.commit()
+        try:
+            phone = request.form.get('phone')
+            entry = User(phone = phone)
+            db.session.add(entry)
+            db.session.commit()
+        except(Exception):
+            db.session.rollback()
 
 
     return render_template('index.html')
+
+
+@app.route("/countries")
+def countries():
+    from Data import get
+    return render_template('countries.html', countries = get())
+
 
 app.run(debug=True)
